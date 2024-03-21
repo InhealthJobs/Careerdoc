1dfcc8eb1598246e7a1f114df0aa1ccf3215e2e6
HENRYPERDIGON <160256854+HENRYPERDIGON@users.noreply.github.com>
2024-03-21 11:17:41 -0500

    Add files via upload

---
+++ extensions.js
@@ -0,0 +1,61 @@
+export const CardExtension = {
+    name: 'Card',
+    type: 'response',
+    match: ({ trace }) =>
+      trace.type === 'ext_card' || trace.payload.name === 'ext_card',
+    render: ({ trace, element }) => {
+      const formContainer = document.createElement('div')
+  
+      formContainer.innerHTML = `
+        <style>
+        .imagen-container {
+            position: relative;
+            width: 200px;
+            height: 200px;
+          }
+          
+          .imagen-container img {
+            width: 100%;
+            height: 100%;
+          }
+          
+          .imagen-hover {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.7);
+            opacity: 0;
+            transition: opacity 0.5s ease-in-out;
+          }
+          
+          .imagen-container:hover .imagen-hover {
+            opacity: 1;
+          }
+          
+          .imagen-hover h1 {
+            color: #ffffff;
+            font-size: 24px;
+            margin-top: 20px;
+            text-align: center;
+          }
+          
+          .imagen-hover p {
+            color: #ffffff;
+            font-size: 16px;
+            margin-top: 10px;
+            text-align: center;
+          }        
+        </style>
+        <div class="imagen-container">
+            <img src="https://ihjsites.blob.core.windows.net/cards/imagenes/ak.webp" alt="Imagen con efecto hover">
+            <div class="imagen-hover">
+                <h1>Arkansas</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere vitae error explicabo dolorum recusandae alias, placeat incidunt vel, aut ipsum id, cupiditate asperiores fuga! Provident!</p>
+            </div>
+        </div> 
+          ` 
+      element.appendChild(formContainer)
+    },
+  }
